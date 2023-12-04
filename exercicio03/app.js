b152d648e88b96f355175ee61bb1164ccb3511e2
FranSou23 <142364644+FranSou23@users.noreply.github.com>
2023-12-04 20:32:44 -0300

    correção

--- exercicio03/app.js
+++ exercicio03/app.js
@@ -2,6 +2,6 @@ function calcular(){
     let numero1, numero2, soma 
     numero1 = document.getElementById("numero1").value 
     numero2 = document.getElementById("numero2").value
-    soma = (parseFloat(numero1) + parseFloat(numero2) 
-    document.getElementById("resposta").innerText = "Resultado"
+    soma = (parseFloat(numero1) + parseFloat(numero2))
+    document.getElementById("resposta").innerText = soma
 }
--- exercicio04/app.js
+++ exercicio04/app.js
@@ -5,5 +5,5 @@ nota2 = document.getElementById("nota2").value
 nota3 = document.getElementById("nota3").value
 nota4 = document.getElementById("nota4").value
 media =(parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3) + parseFloat(nota4))/4
-document.getElementById("resultado"),innerText = "Sua média é: " + media
+calcularmedia = document.getElementById("resultado"),innerText = "Sua média é: " + media
 }
